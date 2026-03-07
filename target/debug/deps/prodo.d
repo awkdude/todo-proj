97b7e3e171f37352a3dd3fe0aa1376e440b20f99
malcolm <Malcolm Lewis>
2026-03-06 20:13:14 -0600

    Removed needed files

--- target/debug/deps/prodo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\malco\Projects\prodo\target\debug\deps\prodo.d: src\main.rs
-
-C:\Users\malco\Projects\prodo\target\debug\deps\prodo.exe: src\main.rs
-
-src\main.rs:
--- target/debug/prodo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\malco\Projects\prodo\target\debug\prodo.exe: C:\Users\malco\Projects\prodo\src\main.rs
